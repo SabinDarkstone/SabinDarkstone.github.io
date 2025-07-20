9e1a2fd95f5f128956102e533b1753033860fdde
Matthew Camp <sabin.darkstone@gmail.com>
2025-07-20 15:20:43 -0400

    Fixing bullets not working

--- site/_plugins/bootstrap_hook.rb
+++ site/_plugins/bootstrap_hook.rb
@@ -10,8 +10,6 @@ Jekyll::Hooks.register :documents, :post_render do |doc|
     # Add Bootstrap classes automatically
     fragment.css('table').each    { |t| t['class'] = (t['class'].to_s.split + ['table','table-striped']).uniq.join(' ') }
     fragment.css('img').each      { |i| i['class'] = (i['class'].to_s.split + ['img-fluid']).uniq.join(' ') }
-    fragment.css('ul').each       { |u| u['class'] = (u['class'].to_s.split + ['list-unstyled']).uniq.join(' ') }
-    fragment.css('ol').each       { |o| o['class'] = (o['class'].to_s.split + ['list-decimal']).uniq.join(' ') }
     fragment.css('pre').each      { |p| p['class'] = (p['class'].to_s.split + ['bg-light','p-3','rounded']).uniq.join(' ') }
     # …add more selectors as needed…
 
