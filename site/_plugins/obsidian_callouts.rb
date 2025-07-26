32b7716253cb3f4af30ffc9447da709bbe9673d2
Matthew Camp <sabin.darkstone@gmail.com>
2025-07-26 16:48:43 -0400

    auto journal commit: 2025-07-26 16:48:43

--- site/_plugins/obsidian_callouts.rb
+++ site/_plugins/obsidian_callouts.rb
@@ -1,29 +1,47 @@
 # site/_plugins/obsidian_callouts.rb
 require "nokogiri"
+require "securerandom"
 
 Jekyll::Hooks.register [:documents, :pages], :post_render do |doc|
   next unless doc.output_ext == ".html"
 
-  # Wrap Obsidian callout blockquotes in a <div class="callout …">
-  html = doc.output
-  html.gsub!(%r{<blockquote>(.*?)</blockquote>}m) do |blk|
-    inner = $1
+  frag = Nokogiri::HTML::DocumentFragment.parse(doc.output)
 
-    # Does the first <p> start with [!TYPE] ?
-    if inner =~ /^\s*<p>\[!(\w+)\]\s*(.*?)<\/p>/m
+  # transform each Obsidian callout (<blockquote>[!TYPE] Title …</blockquote>)
+  frag.css("blockquote").each do |bq|
+    html = bq.inner_html
+
+    # match [!TYPE] Title on first line
+    if html =~ /^\s*<p>\[!(\w+)\]\s*(.*?)<\/p>/m
       type, title = $1.downcase, $2.strip
-      # Remove that first paragraph
-      body = inner.sub(%r{^\s*<p>\[!\w+\].*?<\/p>}m, "")
-      <<~HTML
-        <div class="callout callout-#{type}">
-          <div class="callout-title">#{title}</div>
-          <div class="callout-body">#{body}</div>
+      body_html   = html.sub(%r{^\s*<p>\[!\w+\].*?</p>}m, "")
+
+      # unique ID for collapse
+      cid = "callout-#{SecureRandom.hex(4)}"
+
+      # build the new node
+      new_node = Nokogiri::HTML::DocumentFragment.parse <<~HTML
+        <div class="mb-2">
+          <button
+            class="btn btn-primary"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="##{cid}"
+            aria-expanded="false"
+            aria-controls="#{cid}">
+            #{title}
+          </button>
+          <div id="#{cid}" class="collapse callout callout-#{type}">
+            <div class="callout-body">
+              #{body_html}
+            </div>
+          </div>
         </div>
       HTML
-    else
-      blk
+
+      bq.replace(new_node)
     end
   end
 
-  doc.output = html
+  doc.output = frag.to_html
 end
