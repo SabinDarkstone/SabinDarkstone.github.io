ccf2bc4029021ce2b3c3dd395709b03a257f1a7f
Matthew Camp <sabin.darkstone@gmail.com>
2025-08-25 16:04:27 -0400

    auto journal commit: 2025-08-25 16:04:27

--- site/_plugins/journal_id_permalink.rb
+++ site/_plugins/journal_id_permalink.rb
@@ -1,8 +1,8 @@
 module JournalIdPermalink
     def self.pre_render(doc)
-        return unless doc.respond_to?(:collection) && doc.collection && doc.collection.label == 'journal'
+        return unless doc.respond_to?(:collection) && doc.collection && doc.collection.label.to_s == 'journal'
 
-        id = doc.data['id']
+        id = (doc.data['id'] || doc.data['ID'] || doc.data[':id']).to_s.strip
         return if id.nil? || id.empty?
 
         doc.data['permalink'] = "/journal/#{id}.html"
