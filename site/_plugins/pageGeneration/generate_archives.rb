860fd91fe7819453a4e1ded81bb0c63cde35067d
Matthew Camp <sabin.darkstone@gmail.com>
2025-09-07 15:41:44 -0400

    Fixing breadcrums not working due to missing plugin updates

--- site/_plugins/pageGeneration/generate_archives.rb
+++ site/_plugins/pageGeneration/generate_archives.rb
@@ -4,7 +4,7 @@ require_relative "../helpers/flags"
 
 module PupArchives
     class ArchivePage < Jekyll::PageWithoutAFile
-        def initialize(site, dir, title, entries, permalink)
+        def initialize(site, dir, title, entries, permalink, year, month)
             @site = site
             @base = site.source
             @dir = dir
@@ -17,6 +17,8 @@ module PupArchives
                 "entries" => entries.sort_by { |d| d.data["date"] || Time.at(0) }.reverse,
                 "count" => entries.length,
                 "permalink" => permalink,
+                "year" => year,
+                "month" => month
             }
         end  
     end
@@ -36,7 +38,7 @@ module PupArchives
                 year_title = "Entries from #{year}"
                 year_permalink = "/journal/#{year}/"
                 Jekyll::logger.info "Generating archive page for #{year_title} with #{year_docs.length} entries"
-                site.pages << ArchivePage.new(site, year_dir, year_title, year_docs, year_permalink)
+                site.pages << ArchivePage.new(site, year_dir, year_title, year_docs, year_permalink, year, nil)
                 
                 year_docs.group_by { |d| d.date.strftime("%m") }.each do |month, month_docs|
                     month_name = Date::MONTHNAMES[month.to_i]
@@ -44,7 +46,7 @@ module PupArchives
                     month_title = "#{month_name} #{year} Entries"
                     month_permalink = "/journal/#{year}/#{month}/"
                     Jekyll::logger.info "Generating archive page for #{month_title} with #{month_docs.length} entries"
-                    site.pages << ArchivePage.new(site, month_dir, month_title, month_docs, month_permalink)  
+                    site.pages << ArchivePage.new(site, month_dir, month_title, month_docs, month_permalink, year, month)  
                 end
             end
         end
